cc92752bdf94ece63e50590d7a36939251371dad
bella-cockrell <heybellac@gmail.com>
2020-07-01 16:12:21 +0100

    Adding if/else to catch edge cases

--- solution.js
+++ solution.js
@@ -1,4 +1,15 @@
+// class SolutionTypeError extends Error {
+//   constructor(message) {
+//     super(message);
+//     this.name = 'SolutionTypeError'
+//   }
+// }
+
 const solution = (number) => {
+  if (typeof number === 'object' || typeof number === 'string' || Array.isArray(number)) {
+    console.log(number + ' error: object, string or array');
+    return 0;
+  }
   const flooredLimit = Math.floor(number);
   if (flooredLimit <= 0) {
     return 0;
@@ -14,6 +25,7 @@ const solution = (number) => {
     let fullArray = generateSeries(5, flooredLimit).concat(generateSeries(3, flooredLimit));
     //Set in Javascript doesn't have .reduce so must convert back to array.
     let uniqueValues = new Set(fullArray);
+    console.log(uniqueValues + 'uniqueValues');
     return Array.from(uniqueValues).reduce((total, current) => total + current);
   }
 }
