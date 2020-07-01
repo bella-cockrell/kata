37468bc6dd375e5d6d2f25e8c8a26ac2dfea0fab
bella-cockrell <heybellac@gmail.com>
2020-07-01 11:58:18 +0100

    Updates solution.js to include generateSeries

--- solution.js
+++ solution.js
@@ -1,20 +1,21 @@
 const solution = (number) => {
-  const flooredNumber = Math.floor(number);
-  if (flooredNumber < 3) {
+  const flooredLimit = Math.floor(number);
+  if (flooredLimit <= 0) {
     return 0;
   } else {
-    let array = [];
-    for (let i = 1; i < flooredNumber; i++) {
-      if (i % 3 === 0 || i % 5 === 0) {
+    const generateSeries = (base, limit) => {
+      let array = [];
+      for (let i = 0; i < limit; i += base) {
         array.push(i);
       }
-      array.reduce((total, current) => {
-        let result = total + current;
-        return result;
-      })
-    };
+      return array;
+    }
+
+    let fullArray = generateSeries(5, flooredLimit).concat(generateSeries(3, flooredLimit));
+    //Set in Javascript doesn't have .reduce so must convert back to array.
+    let uniqueValues = new Set(fullArray);
+    return Array.from(uniqueValues).reduce((total, current) => total + current);
   }
 }
-
 
 module.exports = solution;
