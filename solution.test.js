739d29540cd348b9ee0a1dbde1a11be702b6a252
bella-cockrell <heybellac@gmail.com>
2020-07-01 10:52:00 +0100

    Adding edge cases in test

--- solution.test.js
+++ solution.test.js
@@ -1,8 +1,9 @@
 const solution = require('./solution')
 
 test('returns the sum of multiples of 3 or 5 that is less than number', () => {
-  expect(solution(10)).toBe(23);
-  expect(solution(31)).toBe(225); //to remove duplicates like 15 and 30
-  expect(solution(200)).toBe(9168);
-  expect(solution(1000000)).not.toBe(0); //to check speed of calculating large numbers
+  expect(solution(-5)).toBe(0); //handle negative
+  expect(solution(0)).toBe(0); //handle 0
+  expect(solution(2.9)).toBe(0); //handle float
+  expect(solution(10)).toBe(23); //given by challenge
+  expect(solution(200)).toBe(9168); //given by challenge
 })
