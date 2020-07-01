842e0a60dd57d4241c8683aebb2218bad19e4dbf
bella-cockrell <heybellac@gmail.com>
2020-07-01 17:38:49 +0100

    Adding toThrow tests for non-number inputs

--- solution.test.js
+++ solution.test.js
@@ -1,12 +1,17 @@
-const solution = require('./solution')
+const solution = require('./solution');
 
-test('returns the sum of multiples of 3 or 5 that is less than number', () => {
-  expect(solution([])).toBe(0);//handle array
-  expect(solution({})).toBe(0);//handle object
-  expect(solution("Hello")).toBe(0); //handle string
+test('should handle the sum of multiples of 3 or 5 that is less than number', () => {
   expect(solution(-5)).toBe(0); //handle negative
   expect(solution(0)).toBe(0); //handle 0
   expect(solution(2.9)).toBe(0); //handle float
   expect(solution(10)).toBe(23); //given by challenge
   expect(solution(200)).toBe(9168); //given by challenge
+})
+
+test('should handle non-number types', () => {
+  expect(() => { solution([]) }).toThrow();//handle empty array
+  expect(() => { solution([1, 2, 3]) }).toThrow();//handle full array
+  expect(() => { solution({}) }).toThrow();//handle object
+  expect(() => { solution({ firstName: 'Bella' }) }).toThrow();//handle full object
+  expect(() => { solution("Hello") }).toThrow(); //handle string
 })
