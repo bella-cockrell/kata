9a6ffc27867d59eab25df56916b3262aa7df7d74
bella-cockrell <heybellac@gmail.com>
2020-07-01 16:13:16 +0100

    Updating test to include additional non-valid entries

--- solution.test.js
+++ solution.test.js
@@ -1,6 +1,9 @@
 const solution = require('./solution')
 
 test('returns the sum of multiples of 3 or 5 that is less than number', () => {
+  expect(solution([])).toBe(0);//handle array
+  expect(solution({})).toBe(0);//handle object
+  expect(solution("Hello")).toBe(0); //handle string
   expect(solution(-5)).toBe(0); //handle negative
   expect(solution(0)).toBe(0); //handle 0
   expect(solution(2.9)).toBe(0); //handle float
